1eeef711059220e5028fc7e89346f80a4d7aaf03
samoliverschumacher <ssch7766@uni.sydney.ed>
2024-01-25 14:24:08 +1100

    removed non public

--- toyProjects/Property pricing/minimumsquare.m
+++
@@ -1,7 +0,0 @@
-function [base,height] = minimumsquare( numberelements )
-
-height = floor(sqrt(numel(numberelements)))*(rem( (sqrt(numel(numberelements))) , 1)<0.5) + ceil(sqrt(numel(numberelements)))*(rem( (sqrt(numel(numberelements))) , 1)>=0.5);
-base =  ceil(sqrt(numel(numberelements)));
-
-end
-
